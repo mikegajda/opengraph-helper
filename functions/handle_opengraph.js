358902f098539d85a8a2d9f459330ab9eeb5d00d
Mike Gajda <github@mikegajda.com>
2020-06-06 15:17:30 -0400

    LIFE-51: Add fonts to root directory

--- functions/handle_opengraph.js
+++ functions/handle_opengraph.js
@@ -244,8 +244,8 @@ async function processIgStoryImageToBuffer(ogData, ogImage) {
 
 
   // generated with https://ttf2fnt.com/
-  let titleFont = await Jimp.loadFont(currentDir + '/GothicA1-SemiBold-85/GothicA1-SemiBold.ttf.fnt');
-  let urlFont = await Jimp.loadFont(currentDir + '/GothicA1-Regular-50/GothicA1-Regular.ttf.fnt');
+  let titleFont = await Jimp.loadFont(currentDir + '/GothicA1-SemiBold-85.ttf.fnt');
+  let urlFont = await Jimp.loadFont(currentDir + '/GothicA1-Regular-50.ttf.fnt');
 
   let url = extractHostname(ogData.ogUrl)
   let title = ogData.ogTitle
@@ -267,8 +267,8 @@ async function processIgFeedImageToBuffer(ogData, ogImage) {
   let outputImage = background.composite(ogImage, 0, 225);
 
   // generated with https://ttf2fnt.com/
-  let titleFont = await Jimp.loadFont(currentDir + '/GothicA1-SemiBold-50/GothicA1-SemiBold.ttf.fnt');
-  let urlFont = await Jimp.loadFont(currentDir + '/GothicA1-Regular-32/GothicA1-Regular.ttf.fnt');
+  let titleFont = await Jimp.loadFont(currentDir + '/GothicA1-SemiBold-50.ttf.fnt');
+  let urlFont = await Jimp.loadFont(currentDir + '/GothicA1-Regular-32.ttf.fnt');
 
   let url = extractHostname(ogData.ogUrl)
   let title = ogData.ogTitle
@@ -279,16 +279,16 @@ async function processIgFeedImageToBuffer(ogData, ogImage) {
 
 }
 
-// (async () => {
-//   try {
-//     let ogData = await processUrl(
-//         'https://www.nytimes.com/2020/06/05/sports/football/trump-anthem-kneeling-kaepernick.html?action=click&module=Top%20Stories&pgtype=Homepage', true)
-//     // await processIgStoryImageToBuffer(ogData);
-//     // await processIgFeedImageToBuffer(ogData);
-//   } catch (e) {
-//     console.error(e)
-//     // Deal with the fact the chain failed
-//   }
-// })();
+(async () => {
+  try {
+    let ogData = await processUrl(
+        'https://www.nytimes.com/2020/06/05/sports/football/trump-anthem-kneeling-kaepernick.html?action=click&module=Top%20Stories&pgtype=Homepage', true)
+    // await processIgStoryImageToBuffer(ogData);
+    // await processIgFeedImageToBuffer(ogData);
+  } catch (e) {
+    console.error(e)
+    // Deal with the fact the chain failed
+  }
+})();
 
 module.exports.processUrl = processUrl
